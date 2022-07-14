eece2cdc0048d0e77bbe6e1548041c0539071fe1
KohutiakMaksym <makskogutiak@gmail.com>
2022-07-14 17:49:46 +0200

    Finished scanner.js

--- scrape/index.js
+++ scrape/index.js
@@ -1,5 +1,5 @@
-// Set global letiable
-let watchID;
+// Set global variable
+var watchID;
 
 function showPosition() {
     if(navigator.geolocation) {
@@ -13,19 +13,12 @@ function successCallback(position) {
     if(prevLat != position.coords.latitude || prevLong != position.coords.longitude) {
         
         // Set previous location
-        let prevLat = position.coords.latitude;
-        let prevLong = position.coords.longitude;
-        
-        // Get current position
-        let positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
-        document.getElementById("result").innerHTML = positionInfo;
-        
+        var prevLat = position.coords.latitude;
+        var prevLong = position.coords.longitude;
     }
     
 }
 function startWatch() {
-    let result = document.getElementById("result");
-    
         if(watchID) {
             navigator.geolocation.clearWatch(watchID);
             watchID = false;
@@ -34,11 +27,6 @@ function startWatch() {
         }
         document.get
 }
-
-document.querySelector('#scanQR').addEventListener('click', () => {
-    launchScanner();
-    window.scroll(0,findPos(document.querySelector('#scanner')));
-});
 
 function findPos(obj) {
     var curtop = 0;
@@ -52,6 +40,7 @@ function findPos(obj) {
 
 function launchScanner() {
     document.querySelector('#scanner').classList.remove('d-none');
+    window.scroll(0,findPos(document.querySelector('#scanner')));
 }
 
 function hideScanner() {
--- scrape/scanner.js
+++ scrape/scanner.js
@@ -1,10 +1,15 @@
 function onScanSuccess(qrCodeMessage) {
     console.log(qrCodeMessage);
+    if (qrCodeMessage === 'Lion12345'){alert('Congrats, you\'ve unlocked the Lion!')}
+    if (qrCodeMessage === 'Monkey12345'){alert('Congrats, you\'ve unlocked the Monkey!')}
+    if (qrCodeMessage === 'Elephant12345'){alert('Congrats, you\'ve unlocked the Elephant!')}
+    if (qrCodeMessage === 'Bear12345'){alert('Congrats, you\'ve unlocked the Bear!')}
+    if (qrCodeMessage === 'Cock12345'){alert('Congrats, you\'ve unlocked the Cock!')}
 }
 function onScanError(errorMessage) {
     //alert('Error ocurred');
 }
 let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 10, qrbox: 250 });
+    "reader", { fps: 10, qrbox: 300 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
 
